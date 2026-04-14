1b969137f2571c9a473a2c17b489394ee7536893
haonglm <leminhhoang2007pl@icloud.com>
2026-04-15 00:24:36 +0700

    Add files via upload

--- b4/csdl_ss2_b4.sql
+++ b4/csdl_ss2_b4.sql
@@ -1,6 +1,18 @@
+drop database if exists phone_test;
+CREATE DATABASE phone_test;
+USE phone_test;
+
+CREATE TABLE USERS (
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    UserName VARCHAR(50),
+    Phone INT -- Kiểu INT đang bị lỗi mất số 0 đây
+);
+
+INSERT INTO USERS (UserName, Phone) VALUES ('Hoang', 987654321);
+
 ALTER TABLE USERS ADD COLUMN Phone_tmp VARCHAR(15);
-
+SET SQL_SAFE_UPDATES = 0;
 UPDATE USERS SET Phone_tmp = LPAD(Phone, 10, '0');
-
+SET SQL_SAFE_UPDATES = 1;
 ALTER TABLE USERS DROP COLUMN Phone;
 ALTER TABLE USERS RENAME COLUMN Phone_tmp TO Phone;
